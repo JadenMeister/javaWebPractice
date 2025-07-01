f34bbbab87a18854134b0683d251401fbc9df2f3
JadenMeister <bigdragon00@naver.com>
2025-07-01 20:59:10 +0900

    컨트롤러 생성 (유저만)

--- src/main/java/com/example/javaWebPractice/controller/UserController.java
+++ src/main/java/com/example/javaWebPractice/controller/UserController.java
@@ -1,4 +1,27 @@
 package com.example.javaWebPractice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class UserController {
+
+    @GetMapping("/users")
+    public List <User> getAllUsers() {
+        return userService.getAllUsers();
+
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable Long id){
+        return userService.getUserById(id);
+    }
+
+    @PostMapping("/users")
+    public User careateUser(@RequestBody User user) {
+        return userService.save(user);
+    }
 }
