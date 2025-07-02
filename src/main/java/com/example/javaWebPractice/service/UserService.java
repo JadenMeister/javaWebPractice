7791c8df9c9ebff920ba52ec62b2ef52ffb80d0e
JadenMeister <bigdragon00@naver.com>
2025-07-02 12:43:53 +0900

    UserService 수정

--- src/main/java/com/example/javaWebPractice/service/UserService.java
+++ src/main/java/com/example/javaWebPractice/service/UserService.java
@@ -2,8 +2,13 @@ package com.example.javaWebPractice.service;
 
 
 import com.example.javaWebPractice.dto.request.RegisterRequest;
+import com.example.javaWebPractice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.example.javaWebPractice.entity.User;
+import com.example.javaWebPractice.util.PasswordEncoder;
+
 
 import java.util.DuplicateFormatFlagsException;
 
@@ -17,12 +22,18 @@ public class UserService {
     @Autowired
     private EmailService emailService;
 
-    public User createUser(RegisterRequest RegisterRequest){
-        if(userRepository.exitsByUsername(RegisterRequest.getUsername())){
+    public User createUser(RegisterRequest request){
+        if(userRepository.existsByUsername(request.getUsername())){
             throw new DuplicateFormatFlagsException("이미 존재하는 사용자 이름");
         }
 
-        String encodedPassword = PasswordEncoder.encode(RegisterRequest.getPassword());
+//        String encodedPassword = PasswordEncoder.encode(RegisterRequest.getPassword());
+
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setEmail(request.getEmail());
+        user.setRole("USER"); // 기본 역할 설정
 
 
 
